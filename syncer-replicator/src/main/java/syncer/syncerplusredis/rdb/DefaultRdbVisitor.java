b81243d087bc202c1f41b2cc8ff85973ce81e4a9
平行时空 <1604030622@qq.com>
2020-10-19 10:58:11 +0800

    在rdb解析器中指定Module位置

--- syncer-replicator/src/main/java/syncer/syncerplusredis/rdb/DefaultRdbVisitor.java
+++ syncer-replicator/src/main/java/syncer/syncerplusredis/rdb/DefaultRdbVisitor.java
@@ -26,7 +26,7 @@ import syncer.syncerplusredis.util.objectutil.ByteArrayMap;
 import syncer.syncerplusredis.util.objectutil.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import syncer.syncerplusredis.rdb.datatype.Module
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
