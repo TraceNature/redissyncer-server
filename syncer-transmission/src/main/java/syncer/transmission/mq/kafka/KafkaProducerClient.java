4794c308e9bea0311ab3df07e00d43a99f2dd69c
Eq Zhan <35362437+pingxingshikong@users.noreply.github.com>
2021-07-16 23:23:06 +0800

    repairing support for multi command in cluster mode

--- syncer-transmission/src/main/java/syncer/transmission/mq/kafka/KafkaProducerClient.java
+++ syncer-transmission/src/main/java/syncer/transmission/mq/kafka/KafkaProducerClient.java
@@ -9,6 +9,9 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringSerializer;
+import syncer.transmission.util.UrlUtils;
+
+import java.util.Map;
 import java.util.Objects;
 import java.util.Properties;
 @Slf4j
@@ -25,8 +28,22 @@ public class KafkaProducerClient {
     初始化配置
      */
     private Properties initConfig(String servers){
+        String[]data=servers.split("\\?");
+        String address=servers;
         Properties props = new Properties();
-        props.put("bootstrap.servers", servers);
+        if(data.length>1){
+            address=data[0];
+            Map<String,String> params=UrlUtils.urlSplit(servers);
+            String username=params.get("username");
+            String password=params.get("password");
+            String jaasTemplate = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";";
+            String jaasCfg = String.format(jaasTemplate, username, password);
+            props.put("security.protocol","SASL_PLAINTEXT");
+            props.put("sasl.mechanism","SCRAM-SHA-256");
+            props.put("sasl.jaas.config", jaasCfg);
+        }
+
+        props.put("bootstrap.servers", address);
         props.put("acks", "all");
         props.put("retries", 0);
         props.put("batch.size", 16384);
